e50fefbf98372b8e502fd6b4b74231828855718d
RojasCristianE <cristianrg333@gmail.com>
2025-08-13 16:20:23 -0600

    Actualizar URL del script en config.js

--- assets/config.js
+++ assets/config.js
@@ -1,2 +1,2 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxoQ_5Z9g-QlGLsnYT9cUQAvN76Wjg_5NoxvQbGVlQZJ1FndCMf4zh0TBNokdpEpC4jFA/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz47rWk6QD5_PDXodgoUUFHxH4jAJ6i1LaNcpKGk6WN2YwcG5A2_SZr-KXT6fw07_RJiw/exec';
 const REGISTRATION_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSf-e1aINSX51u65wXH--vYvIiGNQWcFCE76Zh6gBvzz-6DOfQ/viewform?usp=header';
