6a7c73461e5b734cb6396520be2543087a34a3a1
RojasCristianE <cristianrg333@gmail.com>
2025-08-13 16:24:27 -0600

    Actualizar URL del script en config.js y eliminar enlaces de verificación en cert.html

--- assets/config.js
+++ assets/config.js
@@ -1,2 +1,2 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz47rWk6QD5_PDXodgoUUFHxH4jAJ6i1LaNcpKGk6WN2YwcG5A2_SZr-KXT6fw07_RJiw/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyZVxzaTf7ByKr79PJkGMC2bojh1lLWZomJ6M7XEYLJkZy6_3kcGxRmUklAfNVMCxAHag/exec';
 const REGISTRATION_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSf-e1aINSX51u65wXH--vYvIiGNQWcFCE76Zh6gBvzz-6DOfQ/viewform?usp=header';
