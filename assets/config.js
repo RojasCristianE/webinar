29c2d6c7286a490e43b841cb0ab24429bdfb0db6
RojasCristianE <cristianrg333@gmail.com>
2025-08-14 23:44:43 -0600

    Actualizar URL del script en config.js, eliminar archivo code.gs y ajustar el script de despliegue

--- assets/config.js
+++ assets/config.js
@@ -1,2 +1,2 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwpXVUtFnr1DjBA9ws75kU5Tc59hqhFeHniprn0hYIUy3P_4Z-Y4Ulazb-hInU2D5zokQ/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxZvH6pmVf2M_q3ef2SosIG7dDGR33lVnY5U8s5X5MLWUkTRbIt981fBU_M6vJNy3_P5Q/exec';
 const REGISTRATION_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSf-e1aINSX51u65wXH--vYvIiGNQWcFCE76Zh6gBvzz-6DOfQ/viewform?usp=header';
