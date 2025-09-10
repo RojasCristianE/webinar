6cfb5e386824ce3eb79a96034ab2254879a65f61
RojasCristianE <cristianrg333@gmail.com>
2025-09-09 23:48:27 -0600

    Actualizar URL del script y agregar nueva sesión 'Del Prompt a la Imágen' en la configuración

--- assets/config.js
+++ assets/config.js
@@ -1,2 +1,2 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby1bd6RwC61RsReynpc5jwXS7i5PpSZOsgfgFoVX2X9IroZ7c2S4rMxqat_qPTP5l59VQ/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyfEgIEpotdXIciyTigbeX8DHxgFlUIC3N91Wjd0EPGSazCFeaaPGC39jg5XJrgmHgNqg/exec';
 const REGISTRATION_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSf-e1aINSX51u65wXH--vYvIiGNQWcFCE76Zh6gBvzz-6DOfQ/viewform?usp=header';
